36c55234a1b6ae1d8caaee554e010cbec5132026
RomainBAU <romain.baudoy696@gmail.com>
2026-03-16 11:28:24 +0100

    add getById

--- src/app/services/CardService.tsx
+++ src/app/services/CardService.tsx
@@ -13,3 +13,18 @@ export async function getAllCards(): Promise<Card[]> {
     return []
   }
 }
+
+export async function getCardById(id: number): Promise<Card | null> {
+  try {
+    const res = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${id}`)
+    if (!res.ok) {
+      throw new Error("Failed to fetch card")
+    }
+    const data = await res.json()
+    return data.data[0] as Card | null
+  } catch (err) {
+    console.error("Error fetching card:", err)
+    return null
+  }
+}
+
